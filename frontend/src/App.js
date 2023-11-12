33852132a0558d28f72d38fba598ea95e2a0dec3
se0jinYoon <tjwls1999@gmail.com>
2023-11-13 05:13:23 +0900

    fix: postForm 컴포넌트 파일 분리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,47 +10,10 @@ import Register from './views/RegisterPage';
 import ProtectedPage from './views/ProtectedPage';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import PostForm from './views/PostForm';
 
 const App = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    content: '',
-    image: null,
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
-  };
-
-  const handleImageChange = (e) => {
-    setFormData({
-      ...formData,
-      image: e.target.files[0],
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let form_data = new FormData();
-    form_data.append('image', formData.image, formData.image.name);
-    form_data.append('title', formData.title);
-    form_data.append('content', formData.content);
-    let url = 'http://localhost:8000/api/posts/';
-
-    axios
-      .post(url, form_data, {
-        headers: {
-          'content-type': 'multipart/form-data',
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => console.log(err));
-  };
+  
 
   return (
     <Router>
@@ -61,40 +24,13 @@ const App = () => {
             <Route path="/protected" element={<ProtectedPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<PostForm />} />
+            {/* <Route path="/post" element={<PostForm />} /> */}
             {/* <PrivateRoute component={ProtectedPage} path="/protected" exact />
             <Route component={Login} path="/login" />
             <Route component={Register} path="/register" />
             <Route component={Home} path="/" /> */}
           </Routes>
-
-          {/* card POST */}
-          <form onSubmit={handleSubmit}>
-            <p>
-              <input
-                type="text"
-                placeholder="Title"
-                id="title"
-                value={formData.title}
-                onChange={handleChange}
-                required
-              />
-            </p>
-            <p>
-              <input
-                type="text"
-                placeholder="Content"
-                id="content"
-                value={formData.content}
-                onChange={handleChange}
-                required
-              />
-            </p>
-            <p>
-              <input type="file" id="image" accept="image/png, image/jpeg" onChange={handleImageChange} required />
-            </p>
-            <input type="submit" />
-          </form>
           {/* <Footer /> */}
         </div>
       </AuthProvider>
---
+++ frontend/src/views/PostForm.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function PostForm() {
+  const [formData, setFormData] = useState({
+    title: '',
+    content: '',
+    image: null,
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    setFormData({
+      ...formData,
+      image: e.target.files[0],
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let form_data = new FormData();
+    form_data.append('image', formData.image, formData.image.name);
+    form_data.append('title', formData.title);
+    form_data.append('content', formData.content);
+    let url = 'http://localhost:8000/api/posts/';
+
+    axios
+      .post(url, form_data, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <input type="text" placeholder="Title" id="title" value={formData.title} onChange={handleChange} required />
+      </p>
+      <p>
+        <input
+          type="text"
+          placeholder="Content"
+          id="content"
+          value={formData.content}
+          onChange={handleChange}
+          required
+        />
+      </p>
+      <p>
+        <input type="file" id="image" accept="image/png, image/jpeg" onChange={handleImageChange} required />
+      </p>
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default PostForm;
